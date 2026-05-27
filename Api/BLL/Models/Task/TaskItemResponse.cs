e9ca497dc05e8da74d999703bcff0c8f025a1fdf
Petro <petrotymchuck@gmail.com>
2026-05-27 09:33:11 +0300

    Add base task models

---
+++ Api/BLL/Models/Task/TaskItemResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BLL.Models.Task;
+
+public class TaskItemResponse
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public DateTime? DueDate { get; set; }
+    public bool IsCompleted { get; set; }
+}
