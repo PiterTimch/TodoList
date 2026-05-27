1da38d269533eb4baa167b35811cd3714a33e20e
Petro <petrotymchuck@gmail.com>
2026-05-27 09:18:45 +0300

    Add TaskEntity

--- Api/DAL/AppDbContext.cs
+++ Api/DAL/AppDbContext.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DAL.Entities.Task;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL;
 
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+    public DbSet<TaskEntity> Tasks { get; set; }
 }
---
+++ Api/DAL/Entities/Base/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Entities.Base;
+
+public abstract class BaseEntity<T> 
+{
+    [Key]
+    public T Id { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+}
